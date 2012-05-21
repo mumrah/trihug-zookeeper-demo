33d4627bd3dd10c12286abaf515fa04ab423597c
David Arthur <mumrah@gmail.com>
2012-05-20 22:01:56 -0400

    Initial checkin of build stuff, libs, and demo #1

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ trihug-zookeeper-demo
 =====================
 
 ZooKeeper demo code for TriHUG May 22, 2012
+
+# Demo One, setting a watch
+
+This example demonstrates the watch mechanism in `ZooKeeper`. All of the basic
+ZooKeeper calls (`create`, `delete`, `exists`, `getData`, `getChildren`) take a
+`Watcher` object. 
+
+The `Watcher` interface defines a single method - `process(WatchedEvent event)`.
+Here, we check for the presence of the znode "/demo".
+
+ zk.exists("/demo", watcher);
+
+This returns a status for the znode and sets a callback on the ZooKeeper server.
+The next time something about "/demo" changes (it gets new children, is deleted,
+etc), the watcher will get notified.
+
+It's important to note that Watchers are one-shot. So if you want continuous
+montitoring of a znode, you need to reset the watch everytime you handle an
+event.
+
+Run this demo with Ant:
+
+ ant clean demo-1
+
+Then in another terminal, connect to the ZooKeeper server and create "/demo"
+with some data:
+
+ create /demo DEMO!
+
+
---
+++ src/demo/DemoOne.java
@@ -0,0 +1,45 @@
+package demo;
+
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+public class DemoOne implements Watcher {
+
+  private volatile boolean _sawDemo = false;
+
+  public boolean sawDemo() {
+    return _sawDemo;
+  }
+
+  @Override
+  public void process(WatchedEvent event) {
+    System.err.println("Event: " + event);
+
+    // Look for a NodeCreate event on /demo
+    if(event.getType() == Watcher.Event.EventType.NodeCreated) {
+      if(event.getPath().equals("/demo")) {
+        _sawDemo = true;
+      } 
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    DemoOne demo = new DemoOne();
+    ZooKeeper zk = new ZooKeeper("localhost:2181", 10000, demo);
+    Stat demoPath= zk.exists("/demo", demo);
+    if(demoPath != null) {
+      System.err.println("Demo exists, deleting it first");
+      zk.delete("/demo", -1);
+      zk.exists("/demo", demo); // Reset the watch
+    }
+    while(true) {
+      if(demo.sawDemo()) {
+        System.err.println("Demo was created!");
+        break;
+      }
+      Thread.sleep(100);
+    }
+  }
+}   
